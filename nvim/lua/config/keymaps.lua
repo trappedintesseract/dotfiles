957bf842d1a71580200c867079f8ecf99ac0b31b
trappedintesseract <sparsh.secret@gmail.com>
2026-01-13 04:57:48 +0530

    Added Readme

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,31 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Molten Keybindings
+vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
+vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "run operator selection" })
+vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
+vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+vim.keymap.set(
+  "v",
+  "<localleader>r",
+  ":<C-u>MoltenEvaluateVisual<CR>gv",
+  { silent = true, desc = "evaluate visual selection" }
+)
+vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
+vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+vim.keymap.set(
+  "n",
+  "<localleader>os",
+  ":noautocmd MoltenEnterOutput<CR>",
+  { silent = true, desc = "show/enter output" }
+)
+vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
+vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+vim.keymap.set(
+  "n",
+  "<localleader>os",
+  ":noautocmd MoltenEnterOutput<CR>",
+  { silent = true, desc = "show/enter output" }
+)
